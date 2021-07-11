eb1fe9911bb1ea044d911b0dc0a14108175a0bb8
Zachary2897 <64550833+Zachary2897@users.noreply.github.com>
2021-07-11 17:00:47 -0400

    MushroomFix v1.0.2
    https://zachary2897.com/post/182767996059/mushroomfix-v102

--- src/main/java/com/zachary2897/mushroomfix/MushroomFix.java
+++ src/main/java/com/zachary2897/mushroomfix/MushroomFix.java
@@ -5,13 +5,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "mushroomfix", name = "MushroomFix", version = "v1.0.1", acceptedMinecraftVersions = "[1.12.2]")
+@Mod(modid = "mushroomfix", name = "MushroomFix", version = "v1.0.2", acceptedMinecraftVersions = "[1.12.2]", clientSideOnly = true)
 public class MushroomFix {
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        Blocks.BROWN_MUSHROOM.setTranslationKey("mushroomBrown");
-        Blocks.BROWN_MUSHROOM_BLOCK.setTranslationKey("mushroomBrown");
-        Blocks.RED_MUSHROOM.setTranslationKey("mushroomRed");
-        Blocks.RED_MUSHROOM_BLOCK.setTranslationKey("mushroomRed");
+        Blocks.BROWN_MUSHROOM.setTranslationKey("brown_mushroom");
+        Blocks.BROWN_MUSHROOM_BLOCK.setTranslationKey("brown_mushroom");
+        Blocks.RED_MUSHROOM.setTranslationKey("red_mushroom");
+        Blocks.RED_MUSHROOM_BLOCK.setTranslationKey("red_mushroom");
     }
 }
