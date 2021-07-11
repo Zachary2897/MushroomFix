27f1ad87a4ea5c13506b6369a31f135c0cc66481
Zachary2897 <64550833+Zachary2897@users.noreply.github.com>
2021-07-11 17:00:05 -0400

    MushroomFix v1.0.1
    https://zachary2897.com/post/181658144009/mushroomfix-v101

--- src/main/java/com/zachary2897/mushroomfix/MushroomFix.java
+++ src/main/java/com/zachary2897/mushroomfix/MushroomFix.java
@@ -4,21 +4,14 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MushroomFix.MODID, name = MushroomFix.NAME, version = MushroomFix.VERSION, acceptedMinecraftVersions = MushroomFix.MCVERSIONS)
+@Mod(modid = "mushroomfix", name = "MushroomFix", version = "v1.0.1", acceptedMinecraftVersions = "[1.12.2]")
 public class MushroomFix {
-    public static final String MODID = "mushroomfix";
-    public static final String NAME = "MushroomFix";
-    public static final String VERSION = "v1.0";
-    public static final String MCVERSIONS = "[1.12.2]";
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {}
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        Blocks.BROWN_MUSHROOM.setUnlocalizedName("mushroomBrown");
-        Blocks.BROWN_MUSHROOM_BLOCK.setUnlocalizedName("mushroomBrown");
-        Blocks.RED_MUSHROOM.setUnlocalizedName("mushroomRed");
-        Blocks.RED_MUSHROOM_BLOCK.setUnlocalizedName("mushroomRed");
+        Blocks.BROWN_MUSHROOM.setTranslationKey("mushroomBrown");
+        Blocks.BROWN_MUSHROOM_BLOCK.setTranslationKey("mushroomBrown");
+        Blocks.RED_MUSHROOM.setTranslationKey("mushroomRed");
+        Blocks.RED_MUSHROOM_BLOCK.setTranslationKey("mushroomRed");
     }
 }
